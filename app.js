b8d52bcb3ed3d71cd3200a156eb15c05b5507953
malik201702 <kilamedazsixab@gmail.com>
2025-04-10 19:14:13 +0400

    html.65

---
+++ app.js
@@ -0,0 +1,9 @@
+let eded = prompt("Bir ədəd daxil edin:");
+
+if (eded < 10) {
+  console.log("10-dan kiçikdir");
+} else if (eded == 10) {
+  console.log("10-a bərabərdir");
+} else {
+  console.log("10-dan böyükdür");
+}
